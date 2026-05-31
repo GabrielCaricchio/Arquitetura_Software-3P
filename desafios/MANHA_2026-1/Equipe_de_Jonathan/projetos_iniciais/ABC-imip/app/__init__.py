cafcc6cb5722ecaa35773b99d5c3fa7e11ea9de2
Jonathan <vicentineraphael@gmail.com>
2026-05-31 16:34:25 -0300

    conectei a OpenAI e fiz os testes utilizando insomnia

--- desafios/MANHA_2026-1/Equipe_de_Jonathan/projetos_iniciais/ABC-imip/app/__init__.py
+++ desafios/MANHA_2026-1/Equipe_de_Jonathan/projetos_iniciais/ABC-imip/app/__init__.py
@@ -7,6 +7,9 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+# importar rota de teste para a AI
+from app.routes.test_ai import test_ai_bp
+
 # Instâncias globais
 db = SQLAlchemy()
 migrate = Migrate()
@@ -35,5 +38,9 @@ def create_app():
     # Registra rotas
     app.register_blueprint(auth_bp)
     app.register_blueprint(quiz_bp)
+    app.register_blueprint(test_ai_bp)
+
+
+    
 
     return app
